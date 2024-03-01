62d28d7e86156fcd77660766495dd1192ef1282d
Admin <Admin@DESKTOP-LD2SIQ1>
2024-03-02 02:30:41 +0400

    Add project files.

---
+++ CountryDataGenerator/Classes/Country.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryDataGenerator.Classes
+{
+    public class CountryName
+    {
+        public string common { get; set; }
+    }
+
+    public class Country
+    {
+        public CountryName name { get; set; }
+        public string region { get; set; }
+        public string subregion { get; set; }
+        public float[] latlng { get; set; }
+        public float area { get; set; }
+        public float population { get; set; }
+
+        public override string ToString()
+        {
+            return $"region:{region};subregion:{subregion};latng:{string.Join(",",latlng)};area:{area};population:{population};\n";
+        }
+    }
+}
---
+++ CountryDataGenerator/Program.cs
@@ -0,0 +1,62 @@
+﻿//მოცემულია Countries REST API ის მისამართი: https://restcountries.com/v3.1/all,
+//რომელიც აბრუნებს ინფორმაციას ქვეყნების შესახებ. დაწერეთ ფუნქცია,
+//რომელიც ყოველი ქვეყნისთვის შექმნის ტექსტურ დოკუმენტს (.txt) სახელად
+//{ქვეყნის_სახელი.txt}. თითოეულ დოკუმენტში უნდა იყოს შევსებული ქვეყნის
+//“region”, “subregion”, “latlng”, “area” და “population” ველები.
+//void GenerateCountryDataFiles();
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using CountryDataGenerator.Classes;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        GenerateCountryDataFiles();
+    }
+
+    static void GenerateCountryDataFiles()
+    {
+        try
+        {
+
+            // URL for the countries API
+            string apiUrl = "https://restcountries.com/v3.1/all";
+
+            // Download the JSON data from the API               
+            string json = new WebClient().DownloadString(apiUrl);            
+
+            //deserialization
+            List<Country> cityList = JsonConvert.DeserializeObject<List<Country>>(json);
+            //file creation
+            foreach (Country item in cityList)
+            {
+                string path = @"C:\Users\Admin\OneDrive\Desktop\g\demo-repo\CountryDataGenerator\CountryDataGenerator\GeneratedFiles\";
+                string filename = $"{path}{item.name.common}.txt";
+                string filecontent = item.ToString();
+                if (File.Exists(filename))
+                    File.Delete(filename);
+
+                using (FileStream fs = File.Create(filename))
+                {
+                    Byte[] content = new UTF8Encoding(true).GetBytes(filecontent);
+                    fs.Write(content, 0, content.Length);
+                }
+                //Console.WriteLine(item);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error:{ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("Files have already created!");
+        }
+    }
+}
